db47d37bb9b6deddd52a74ad4b8b67a7bd68d0fd
RamiroNB <ramiro.nilson.barros@gmail.com>
2023-11-25 14:43:05 -0300

    Delete some stuff and add programa basico 3d

--- include/Linha.h
+++ include/Linha.h
@@ -1,14 +1,26 @@
-#pragma once
 
-#include "Ponto.h"
+
+#ifndef Linha_h
+#endif
+#define Linha_h
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#endif
+
+#ifdef __linux__
+#include <GL/glut.h>
+#endif
+
+// #include "Ponto.h"
 
 class Linha
 {
-    float minx, miny, maxx, maxy; // envelope
+	float minx, miny, maxx, maxy; // envelope
 
 public:
-    float x1, y1, x2, y2;
+	float x1, y1, x2, y2;
 
-    void geraLinha(int limite, int TamMax);
-    void desenhaLinha();
+	void geraLinha(int limite, int TamMax);
+	void desenhaLinha();
 };
